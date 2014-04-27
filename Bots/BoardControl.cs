2bec4db86f31660d06ad72b4b6bc698b8c8d40e0
Juce <jucemessage@gmail.com>
2014-04-27 07:28:41 +0200

    Custom Class: BoardControl
    A custom class that is designed to clear the board instead of rushing
    the enemy.

---
+++ Bots/BoardControl.cs
@@ -0,0 +1,43 @@
+﻿using HRCustomClasses;
+using HREngine.API;
+using HREngine.API.Actions;
+using HREngine.API.Utilities;
+using HREngine.Basics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HREngine.Bots
+{
+   public class BoardControl : HREngine.Basics.BasicBot
+   {
+      public BoardControl()
+      {
+        HRLog.Write("BoardControl custom class initialized.");
+      }
+
+      protected override HREntity GetNextAttackToAttack()
+      {
+         var enemyState = new PlayerState(HRPlayer.GetEnemyPlayer());
+         if (enemyState.Minions > 0)
+         {
+            var list = enemyState.TauntMinions;
+            if (list.Count == 0)
+            {
+               list = enemyState.AttackableMinions;
+            }
+
+            var sorting = new Sorting();
+            sorting.SortByHealth(ref list);
+
+            foreach (var minion in list)
+            {
+               if (minion.CanBeAttacked())
+                  return minion;
+            }
+         }
+
+         return enemyState.Player.GetHero();
+      }    
+   }
+}
