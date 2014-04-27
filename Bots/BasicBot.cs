701f571606ac3a8a041664846cd499a272088204
Juce <jucemessage@gmail.com>
2014-04-27 07:28:24 +0200

    Custom Class: Basic Bot
    The built in bot which is designed to work with smart decks.

---
+++ Bots/BasicBot.cs
@@ -0,0 +1,17 @@
+﻿using HRCustomClasses;
+using HREngine.API;
+using HREngine.API.Actions;
+using HREngine.API.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HREngine.Bots
+{
+   public class BasicBot : HREngine.Basics.BasicBot
+   {
+      public BasicBot()
+      {
+      }
+   }
+}
