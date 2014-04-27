38ba61460be5c3e713cca6eb3e6eb3568e65c4e3
Juce <jucemessage@gmail.com>
2014-04-27 07:28:03 +0200

    Basic Code of internals - shared for the public.
    This code contains the logic of the built in custom class "BasicBot" and
    a small utility to sort values just to publish an example how to solve
    that.

---
+++ Basics/Sorting.cs
@@ -0,0 +1,84 @@
+﻿using HREngine.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HRCustomClasses
+{
+   public class Sorting
+   {
+      public Sorting()
+      {
+      }
+
+      public enum SortMode
+      {
+         Ascending = 0,
+         Descending = 1
+      };
+
+      public void SortByCost(ref List<HRCard> set, SortMode order = SortMode.Ascending)
+      {
+         set.Sort(delegate(HRCard X, HRCard Y)
+         {
+            if (order == SortMode.Ascending)
+            {
+               if (X.GetEntity().GetCost() < Y.GetEntity().GetCost())
+                  return -1; // Y is Greater
+
+               return 1; // X is Greater
+            }
+            else
+            {
+               if (X.GetEntity().GetCost() > Y.GetEntity().GetCost())
+                  return 1;
+
+               return -1;
+            }
+         });
+      }
+
+      public void SortByHealth(ref List<HREntity> set, SortMode order = SortMode.Ascending)
+      {
+         set.Sort(delegate(HREntity X, HREntity Y)
+         {
+            if (order == SortMode.Ascending)
+            {
+               if (X.GetRemainingHP() < Y.GetRemainingHP())
+                  return -1; // Y is Greater
+
+               return 1; // X is Greater
+            }
+            else
+            {
+               if (X.GetRemainingHP() > Y.GetRemainingHP())
+                  return 1;
+
+               return -1;
+            }
+         });
+      }
+
+      public void SortByAttack(ref List<HREntity> set, SortMode order = SortMode.Ascending)
+      {
+         set.Sort(delegate(HREntity X, HREntity Y)
+         {
+            if (order == SortMode.Ascending)
+            {
+               if (X.GetATK() < Y.GetATK())
+                  return -1; // Y is Greater
+
+               return 1; // X is Greater
+            }
+            else
+            {
+               if (X.GetATK() > Y.GetATK())
+                  return 1;
+
+               return -1;
+            }
+         });
+      }
+   }
+}
